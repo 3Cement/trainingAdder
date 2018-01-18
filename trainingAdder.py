6d3a09e10fce5a4473684dfe172c121917afd772
Daniel <30894056+3Cement@users.noreply.github.com>
2018-01-18 20:49:56 +0000

    Update trainingAdder.py

--- trainingAdder.py
+++ trainingAdder.py
@@ -2,58 +2,53 @@
 # TrainingAdder.py - The program that automatically logging into Endomondo website.
 # Then find the latest training on the hard drive and add that to the Endomondo profile
 
-import time, webbrowser, pdb
+import time, webbrowser, pdb, glob, os
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+# Finding the lates added file in the folder:
+list_of_files = glob.glob("C:\\Users\\Daniel\\OneDrive\\Moje treningi\\*.gpx")
+latest_file = max(list_of_files, key=os.path.getctime)
+print(latest_file)
 
 browser = webdriver.Chrome(r'C:\Users\Daniel\MyPythonScripts\webdrivers\chromedriver_win32\chromedriver.exe')
 browser.get('https://www.endomondo.com/login?returnUrl=~2Fhome')
-#time.sleep(5) # Let the user actually see something!
+#browser.maximize_window()
+time.sleep(1)
 
 emailElem = browser.find_element_by_name('email')
-emailElem.send_keys('*****@gmail.com')
-passwordElem = browser.find_element_by_name('*****')
-passwordElem.send_keys('mojetreningi123')
+emailElem.send_keys('******')
+passwordElem = browser.find_element_by_name('password')
+passwordElem.send_keys('******')
 passwordElem.submit()
-
 time.sleep(1)
+
 browser.get('https://www.endomondo.com/workouts/create')
 
 fileImport = browser.find_element_by_class_name('fileImport')
 fileImport.click()
 
-path = u'//a[text()="Choose File"]'
+time.sleep(1)
+browser.switch_to.frame(browser.find_element_by_tag_name("iframe"))
+time.sleep(1)
+
+try:
+	element = browser.find_element_by_name("uploadFile")
+	#element.click()
+	element.clear()
+	time.sleep(1)
+	element.send_keys(latest_file)
+except Exception as exc:
+	print('There was a problem: %s' % (exc))
+
+try:
+	uploadFile = browser.find_element_by_name("uploadSumbit").click()
+except Exception as exc:
+	print('There was a problem: %s' % (exc))
 
 time.sleep(1)
-#pdb.set_trace()
 try:
-	fileUpload = browser.find_element_by_xpath(path).click()
+	nextUpload = browser.find_element_by_name("reviewSumbit").click()
 except Exception as exc:
-	print('There was a 1st problem: %s' % (exc))
-try:
-	fileUpload = browser.find_element_by_class_name(path).click()
-except Exception as exc:
-	print('There was a 2nd problem')
-try:
-	fileUpload = browser.find_element_by_css_selector(path).click()	
-except Exception as exc:
-	print('There was a 3rd problem')
-try:
-	fileUpload = browser.find_element_by_id(path).click()	
-except Exception as exc:
-	print('There was a 4th problem')
-try:
-	fileUpload = browser.find_element_by_link_text(path).click()	
-except Exception as exc:
-	print('There was a 5th problem')
-try:
-	fileUpload = browser.find_element_by_partial_link_text(path).click()	
-except Exception as exc:
-	print('There was a 6th problem')
-try:
-	fileUpload = browser.find_element_by_name(path).click()	
-except Exception as exc:
-	print('There was a 7th problem: %s' % (exc))
-try:
-	fileUpload = browser.find_element_by_tag_name(path).click()	
-except Exception as exc:
-	print('\n' + path)
+	print('There was a problem: %s' % (exc))
